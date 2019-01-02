3f03c74504770bd7357c0de862c90fb3622052bb
mjsumpter <mjsumpter@gmail.com>
2019-01-01 23:21:20 -0500

    began collecting climbing slang and quotes

--- src/generator.js
+++ src/generator.js
@@ -6,9 +6,86 @@ const loremIpsum = new GenerateText();
 //constructor function taht creats an object with sentences property array (this.sentences = [])
 function GenerateText() {
     this.sentences = [
-        //add climber sentences
+        "Choo choo, here comes the send train.",
+        "The crack was super splitter",
+        "I was having a high gravity day.",
+        "It goes, boys.",
+        
+        //Rawk Tawk instagam.com/rawk_tawk
+        "He's been in J Tree soul shredding on sandbagged lines without pads or shoes.",
+        "Flexin' the new secret crag.",
+        "The best line I have seen.",
+        "I found this amazing new project with the best holds.",
+        "Different beta works for different people.",
+        "I'm working on this sick proj right now.",
+        "It has this crazy toe hook.",
+        "Does climbing V10 in the gym still get you sponsored?",
+        "There is no such thing as a rose move, you can always just match.",
+        "It would be rude of me to not offer beta.",
+        "Homie, you can dip in my chalk.",
+        "The difference between an arete and a lip traverse? Only perspective."
+        "I've got kneebar beta for your proj.",
+        "At some point we're going to have to stop calling every line with a toe hook or a handflip futuristic.",
+        "Climbing is either a beautiful gift or a curse.",
+        "I wonder why my climbing partner only invites me on highball days.",
+        "When you're older than 30, every season is comeback season.",
+        "In climbing there is only one rule: the ground is off.",
+
+
     ];
 }
+
+//Generate random sentences from climber words, mad lib style
+let climberSlang = [
+    "beta",
+    "toe hook",
+    "drop knee",
+    "trad daddy",
+    "crimp",
+    "dyno",
+    "off the deck",
+    "chalk up",
+    "shredding the gnar",
+    "gnar gnar",
+    "van life",
+    "lowball",
+    "high ball",
+    "free solo",
+    "The Red",
+    "The New",
+    "Hueco",
+    "huecos",
+    "crimp",
+    "pinch",
+    "sloper",
+    "chossy",
+    "mad choss",
+    "abseil",
+    "barn door",
+    "bashies",
+    "beta",
+    "biner",
+    "bomber",
+    "crag",
+    "crux",
+    "high friction",
+    "on-sight",
+    "red point",
+    "pumped",
+    "gumby",
+    "permagumby",
+    "beta-mime",
+    "dirtbag",
+    "crush rocks",
+    "crushing",
+    "it goes",
+];
+
+let proClimbers = [
+    "Dave Graham",
+    "Chris Sharma",
+    "Alex Puccio",
+];
 //Method to GenreateNewText constructor that generates a random sentence
 
 // Method to GenerateNewText constructor that generates a paragraph from random sentences
